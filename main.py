a454f836bfc307e7b0ee7cc96fd4298ac2520481
elnukakujo <jagernoe1s3@hotmail.com>
2024-08-12 19:42:36 -0400

    Added one hot encoding

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import numpy as np
 from display import display_img
 from preprocess import preprocess
 
-x_train, y_train, x_test, y_test = preprocess(load=True)
+x_train, y_train, x_test, y_test = preprocess(load=False)
 print(x_train)
--- preprocess.py
+++ preprocess.py
@@ -21,6 +21,11 @@ def image_normalization(image):
     image = tf.cast(image, tf.float32) / 255.0
     return tf.transpose(image)
 
+def one_hot_encoder(labels):
+    num_classes = tf.unique(labels).y.shape[0]
+    one_hot=tf.one_hot(labels,depth=num_classes)
+    return tf.transpose(one_hot)
+
 def preprocess(load=False):
     if load:
         return (load_weights(f'data/processed/x_train.npy'), load_weights(f'data/processed/y_train.npy'),
@@ -29,6 +34,7 @@ def preprocess(load=False):
     x_train, y_train = split_tensor(train)
     x_test, y_test = split_tensor(test)
     x_train, x_test = image_normalization(x_train),image_normalization(x_test)
+    y_train, y_test = one_hot_encoder(y_train), one_hot_encoder(y_test)
     save_weights(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
     
     return x_train, y_train, x_test, y_test
